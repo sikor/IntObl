ca6a81d4e62f3e2560b6a709ac6f08e1e5eadf13
pawel <siegfriedek@gmail.com>
2014-10-21 22:00:47 +0200

    runner

--- src/runner.py
+++ src/runner.py
@@ -1,7 +1,8 @@
 from template import solver
+from zdt_problems import Zdt1
 
 __author__ = 'pawel'
 
 if __name__ == '__main__':
-    problem = None
+    problem = Zdt1()
     solver(problem, population_size=100, generations_num=500, verbose=True, chart=True, weights=(-1.0, -1.0))
