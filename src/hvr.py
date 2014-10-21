1454bc01ab4733053da936dd81151535ab77700f
Grzegorz Wilaszek <wilaszekg@gmail.com>
2014-10-21 22:33:57 +0200

    hv metric

--- src/hvr.py
+++ src/hvr.py
@@ -1,6 +1,17 @@
+from functools import reduce
+
+
+def mul(elements):
+    return reduce(lambda x, y: x * y, elements)
+
+
 def hv(solution, w):
-    return sum([])
+    return sum([volume(point, w) for point in solution])
 
 
 def volume(point, w):
-    return sum([x1 + x2 for x1, x2 in zip(point, 2)])
+    return mul([abs(x1 - x2) for x1, x2 in zip(point, 2)])
+
+
+def zdt_hv(solution):
+    return hv(solution, [0.5] * 30)
