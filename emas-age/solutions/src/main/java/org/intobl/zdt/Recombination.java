4d9def85596e0a930dba23ae81f61018e94afdb8
Grzegorz Wilaszek <wilaszekg@gmail.com>
2014-11-04 18:05:09 +0100

    Implemented Recombination of ZDT for AGE

--- emas-age/solutions/src/main/java/org/intobl/zdt/Recombination.java
+++ emas-age/solutions/src/main/java/org/intobl/zdt/Recombination.java
@@ -3,13 +3,34 @@ package org.intobl.zdt;
 import org.jage.solution.ISolution;
 import org.jage.variation.recombination.IRecombine;
 
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by pawel on 04/11/14.
  */
 public class Recombination implements IRecombine<ISolution> {
 
-    @Override
-    public void recombine(ISolution solution1, ISolution solution2) {
+    private Random random = new Random();
 
+    @Override
+    public void recombine(ISolution first, ISolution second) {
+        ZdtSolution firstZdt = (ZdtSolution) first;
+        ZdtSolution secondZdt = (ZdtSolution) second;
+
+        List<Double> firstZdtRepresentation = firstZdt.getRepresentation();
+        List<Double> secondZdtRepresentation = secondZdt.getRepresentation();
+        for (int i = 0; i < firstZdtRepresentation.size(); i++) {
+            double a, b;
+            if (random.nextBoolean()) {
+                a = firstZdtRepresentation.get(i);
+                b = secondZdtRepresentation.get(i);
+            } else {
+                b = firstZdtRepresentation.get(i);
+                a = secondZdtRepresentation.get(i);
+            }
+            firstZdtRepresentation.set(i, a);
+            secondZdtRepresentation.set(i, b);
+        }
     }
 }
