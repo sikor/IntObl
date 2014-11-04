d5ed114156e23128bc52e1fe4c972dfee38a9b28
pawel <siegfriedek@gmail.com>
2014-11-04 17:01:07 +0100

    ZdtSolution interface update

--- emas-age/solutions/src/main/java/org/intobl/zdt/ZdtSolution.java
+++ emas-age/solutions/src/main/java/org/intobl/zdt/ZdtSolution.java
@@ -1,16 +1,16 @@
 package org.intobl.zdt;
 
-import org.jage.solution.ISolution;
+import org.jage.solution.IVectorSolution;
+
+import java.util.List;
 
 /**
  * Created by pawel on 04/11/14.
  */
-public class ZdtSolution extends SolutionWithFitness implements ISolution {
-
-
+public class ZdtSolution extends SolutionWithFitness implements IVectorSolution<Double> {
 
     @Override
-    public Object getRepresentation() {
+    public List<Double> getRepresentation() {
         return null;
     }
 }
