b1b902c68dd69c21da66a852cbdfc57897913461
pawel <siegfriedek@gmail.com>
2014-11-04 16:39:09 +0100

    Added ZdtSolution class stub.

--- emas-age/solutions/src/main/java/org/intobl/zdt/ZdtSolution.java
+++ emas-age/solutions/src/main/java/org/intobl/zdt/ZdtSolution.java
@@ -1,7 +1,13 @@
 package org.intobl.zdt;
 
+import org.jage.solution.ISolution;
+
 /**
  * Created by pawel on 04/11/14.
  */
-public class ZdtSolution {
+public class ZdtSolution implements ISolution {
+    @Override
+    public Object getRepresentation() {
+        return null;
+    }
 }
