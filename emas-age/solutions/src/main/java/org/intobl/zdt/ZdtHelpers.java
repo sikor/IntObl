d546835aa759f20fa0d77a4f0e25c2761ef49609
pawel <siegfriedek@gmail.com>
2014-11-04 17:23:28 +0100

    calculating Zdt1Fitness

---
+++ emas-age/solutions/src/main/java/org/intobl/zdt/ZdtHelpers.java
@@ -0,0 +1,42 @@
+package org.intobl.zdt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by pawel on 04/11/14.
+ */
+public class ZdtHelpers {
+
+    public static final int N = 30;
+
+
+    public static List<Double> zdt1Fitness(ZdtSolution solution) {
+        List<Double> representation = solution.getRepresentation();
+        List<Double> result = new ArrayList<Double>(2);
+
+        double f1 = representation.get(0);
+
+        double tailSum = 0;
+        for (int i = 1; i < representation.size(); ++i) {
+            tailSum += representation.get(i);
+        }
+
+        double g = 1 + (9 / (N - 1)) * tailSum;
+
+        double f2 = g * zdt1H(f1, g);
+
+        result.add(f1);
+        result.add(f2);
+
+        return result;
+    }
+
+    private static double zdt1H(double f1, double g) {
+        return 1 - Math.sqrt(f1 / g);
+    }
+
+    private ZdtHelpers() {
+
+    }
+}
